6907ef49821e395bc3d15fcfe3aaa65399d906d9
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-10-01 15:50:38 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Mono.Cecil;
+
+namespace awegwaawehg {
+	class MainClass {
+		public static void Main (string [] args)
+		{
+			Check ("/Users/rolf/work/maccore/main/xamarin-macios/_ios-build/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/Xamarin.iOS.dll");
+			Check ("/Users/rolf/work/maccore/xcode12.2/xamarin-macios/_ios-build/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/Xamarin.iOS.dll");
+
+			Console.WriteLine ("Hello World!");
+		}
+
+		static void Check (string filename)
+		{
+			Console.WriteLine (filename);
+			var asm = AssemblyDefinition.ReadAssembly (filename);
+			var n = "QuickLook.IQLPreviewItem";
+			var t = asm.MainModule.GetType (n);
+			Console.WriteLine (t.CustomAttributes.Count);
+		}
+	}
+}
