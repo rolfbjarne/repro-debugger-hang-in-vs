1c2cc82d6e6831df6fc3dbbfa6a416341b3484f0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-10-01 17:44:56 +0200

    Test case

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 using Mono.Cecil;
 
@@ -6,16 +7,23 @@ namespace awegwaawehg {
 	class MainClass {
 		public static void Main (string [] args)
 		{
-			Check ("/Users/rolf/work/maccore/main/xamarin-macios/_ios-build/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/Xamarin.iOS.dll");
-			Check ("/Users/rolf/work/maccore/xcode12.2/xamarin-macios/_ios-build/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/Xamarin.iOS.dll");
+			var dir = Path.GetDirectoryName (System.Reflection.Assembly.GetExecutingAssembly ().Location);
+			string path = null;
+			do {
+				path = Path.Combine (dir, "Xamarin.iOS.dll");
+				if (File.Exists (path))
+					break;
+				dir = Path.GetDirectoryName (dir);
+			} while (true);
 
-			Console.WriteLine ("Hello World!");
+
+			Check (path);
 		}
 
 		static void Check (string filename)
 		{
 			Console.WriteLine (filename);
-			var asm = AssemblyDefinition.ReadAssembly (filename);
+			var asm = AssemblyDefinition.ReadAssembly (filename, new ReaderParameters { ReadingMode = ReadingMode.Deferred });
 			var n = "QuickLook.IQLPreviewItem";
 			var t = asm.MainModule.GetType (n);
 			Console.WriteLine (t.CustomAttributes.Count);
